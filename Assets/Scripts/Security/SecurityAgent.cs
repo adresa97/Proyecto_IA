874d1c7bf4bcef672afd6f5f941ac7cdfe5e8dab
adresa97 <adriresa97@gmail.com>
2024-01-18 02:48:31 +0100

    Training with obstacles after 9 versions

--- Assets/Scripts/Security/SecurityAgent.cs
+++ Assets/Scripts/Security/SecurityAgent.cs
@@ -8,6 +8,9 @@ using Unity.MLAgents.Actuators;
 using UnityEngine.Experimental.AI;
 using UnityEngine.AI;
 using System.Runtime.CompilerServices;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
+using System;
 
 public class SecurityAgent : Agent
 {
@@ -15,6 +18,8 @@ public class SecurityAgent : Agent
     private Transform target;
     [SerializeField]
     private Transform floor;
+    [SerializeField]
+    private float targetPadding;
 
     [SerializeField]
     private float angularSpeed;
@@ -27,7 +32,8 @@ public class SecurityAgent : Agent
     private Quaternion initialRotation;
     private Rigidbody rBody;
 
-    private float floorLeftBorder, floorRightBorder, floorTopBorder, floorBottomBorder;
+    private float floorWorkingSizeX;
+    private float floorWorkingSizeZ;
 
     private bool isAtTarget;
 
@@ -37,10 +43,11 @@ public class SecurityAgent : Agent
         initialRotation = transform.rotation;
         rBody = GetComponent<Rigidbody>();
 
-        floorLeftBorder = floor.position.x - floor.localScale.x/2;
-        floorRightBorder = floor.position.x + floor.localScale.x/2;
-        floorTopBorder = floor.position.z + floor.localScale.z/2;
-        floorBottomBorder = floor.position.z - floor.localScale.z/2;
+        floorWorkingSizeX = floor.localScale.x;
+        if (targetPadding < floor.localScale.x / 2) floorWorkingSizeX -= 2 * targetPadding;
+
+        floorWorkingSizeZ = floor.localScale.z;
+        if (targetPadding < floor.localScale.z / 2) floorWorkingSizeZ -= 2 * targetPadding;
 
         isAtTarget = false;
     }
@@ -60,32 +67,21 @@ public class SecurityAgent : Agent
         isAtTarget = false;
 
         // Relocate target
-        Vector3 newPosition = Vector3.zero;
-        newPosition.x = floorLeftBorder + Random.value * floor.localScale.x;
-        newPosition.z = floorBottomBorder + Random.value * floor.localScale.z;
-
-        NavMeshHit hit;
-        NavMesh.SamplePosition(newPosition, out hit, floor.localScale.x/2, 1);
-        newPosition = hit.position;
-
-        newPosition.y = target.position.y;
-        target.position = newPosition;
+        target.position = RandomPositionOnFloor(floor);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         // Agent and target positions
-        sensor.AddObservation(new Vector2((target.position.x - floorLeftBorder) / (floorRightBorder - floorLeftBorder),
-                                          (target.position.z - floorBottomBorder) / (floorTopBorder - floorBottomBorder)));
-        sensor.AddObservation(new Vector2((transform.position.x - floorLeftBorder) / (floorRightBorder - floorLeftBorder),
-                                          (transform.position.z - floorBottomBorder) / (floorTopBorder - floorBottomBorder)));
+        sensor.AddObservation(NormalizedPosition(target.position));
+        sensor.AddObservation(NormalizedPosition(transform.position));
 
-        sensor.AddObservation((target.localPosition - transform.localPosition).normalized);
+        // Agent direction
         sensor.AddObservation(transform.forward);
 
-        // Velocities
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
+        // Velocities, normalized to [-1, 1]
+        sensor.AddObservation(rBody.velocity.x / maxSpeed);
+        sensor.AddObservation(rBody.velocity.z / maxSpeed);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -93,8 +89,8 @@ public class SecurityAgent : Agent
         // Actions
         // Discrete action 0: rotation: 1 -> static, 2 -> rotate right, 3 -> rotate left
         int rotateDirection = 0;
-        if (actions.DiscreteActions[0] == 2) rotateDirection = 1;
-        else if (actions.DiscreteActions[0] == 3) rotateDirection = -1;
+        if (actions.DiscreteActions[0] == 1) rotateDirection = 1;
+        else if (actions.DiscreteActions[0] == 2) rotateDirection = -1;
 
         Vector3 rotation = transform.rotation.eulerAngles;
         rotation.y += rotateDirection * angularSpeed * Time.deltaTime;
@@ -102,8 +98,8 @@ public class SecurityAgent : Agent
 
         // Discrete action 1: movement: 1 -> static, 2 -> move forwward, 3 -> move backward
         int moveDirection = 0;
-        if (actions.DiscreteActions[1] == 2) moveDirection = 1;
-        else if (actions.DiscreteActions[1] == 3) moveDirection = -1;
+        if (actions.DiscreteActions[1] == 1) moveDirection = 1;
+        else if (actions.DiscreteActions[1] == 2) moveDirection = -1;
 
         rBody.AddForce(transform.forward * moveDirection * speed);
         if (rBody.velocity.magnitude > maxSpeed) rBody.velocity = rBody.velocity.normalized * maxSpeed;
@@ -117,7 +113,7 @@ public class SecurityAgent : Agent
         }
 
         // Fell out floor
-        else if (transform.position.y < 0)
+        else if (transform.position.y < floor.position.y + floor.localScale.y/2)
         {
             SetReward(-1.0f);
             EndEpisode();
@@ -126,21 +122,53 @@ public class SecurityAgent : Agent
 
     public void OnTriggerStay(Collider other)
     {
+        // If activates trigger at target gameObject
         if (other.gameObject == target.gameObject) isAtTarget = true;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        int rotationOutput = 1;
-        if (!Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) rotationOutput = 2;
-        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) rotationOutput = 3;
+        // Decide rotation state: 1 -> no input, 2 -> right arrow, 3 -> left arrow
+        int rotationOutput = 0;
+        if (!Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) rotationOutput = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) rotationOutput = 2;
 
-        int movementOutput = 1;
-        if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow)) movementOutput = 2;
-        else if (!Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow)) movementOutput = 3;
+        // Decide movement state: 1 -> no input, 2 -> up arrow, 3 -> down arrow
+        int movementOutput = 0;
+        if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow)) movementOutput = 1;
+        else if (!Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow)) movementOutput = 2;
 
+        // Store outputs in discrete actions
         var discreteActionsOut = actionsOut.DiscreteActions;
         discreteActionsOut[0] = rotationOutput;
         discreteActionsOut[1] = movementOutput;
+    }
+
+    private Vector3 RandomPositionOnFloor(Transform floor)
+    {
+        // Get a random position on floor
+        Vector3 randomPosition = Vector3.zero;
+        randomPosition.x = floor.position.x + UnityEngine.Random.value * floorWorkingSizeX - floorWorkingSizeX / 2;
+        randomPosition.z = floor.position.z + UnityEngine.Random.value * floorWorkingSizeZ - floorWorkingSizeZ / 2;
+
+        // Check if position is walkable (get nearest walkable position if not)
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomPosition, out hit, floorWorkingSizeX / 2, 1);
+        randomPosition = hit.position;
+
+        // Keep target y position value
+        randomPosition.y = target.position.y;
+
+        return randomPosition;
+    }
+
+    private Vector2 NormalizedPosition(Vector3 position)
+    {
+        // Turn 3 dimension position to 2 dimension position in range from -1 to 1
+        Vector2 normalizedPosition = Vector2.zero;
+        normalizedPosition.x = 2 * (position.x - floor.position.x) / floor.localScale.x;
+        normalizedPosition.y = 2 * (position.z - floor.position.z) / floor.localScale.z;
+
+        return normalizedPosition;
     }
 }
